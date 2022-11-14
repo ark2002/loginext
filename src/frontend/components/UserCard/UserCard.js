c36b45f1ed4d58582b06746246d774b09202e91b
Aryak Lahane <aryakprakashlahane@gmail.com>
2022-11-14 16:35:26 +0530

    refactor : updated usercard

--- src/frontend/components/UserCard/UserCard.js
+++ src/frontend/components/UserCard/UserCard.js
@@ -1,33 +1,32 @@
 import "./UserCard.css";
 
-const UserCard = () => {
+const UserCard = ({ user }) => {
+  const { profileImg, fullname, email, phone, website, liked } = user;
   return (
     <div className="user__card flex__column">
-      <img
-        className="user__img"
-        src="https://picsum.photos/400/250"
-        alt="user__img"
-      />
+      <img className="user__img" src={profileImg} alt="user__img" />
       <div className="user__details-container flex__column">
-        <p className="text__large">John Doe</p>
+        <p className="text__large">{fullname}</p>
         <div className="user__details flex__row-center">
           <span className="material-icons-outlined icon">mail</span>
-          <p className="text__small">abc&gmail.com</p>
+          <p className="text__small">{email}</p>
         </div>
         <div className="user__details flex__row-center">
           <span className="material-icons-outlined icon">call</span>
-          <p className="text__small">+91-8369910242</p>
+          <p className="text__small">{phone}</p>
         </div>
         <div className="user__details flex__row-center">
           <span className="material-icons-outlined icon">language</span>
-          <p className="text__small">gmail.com</p>
+          <p className="text__small">{website}</p>
         </div>
       </div>
       <div className="user__choices flex__row-center">
-          <span className="material-icons favorite">favorite_outline</span>
-          <span className="material-icons-outlined icon edit">edit</span>
-          <span className="material-icons-outlined icon delete" >delete</span>
-        </div>
+        <span className="material-icons favorite">
+          {liked ? "favorite" : "favorite_outline"}
+        </span>
+        <span className="material-icons-outlined icon edit">edit</span>
+        <span className="material-icons-outlined icon delete">delete</span>
+      </div>
     </div>
   );
 };
