7cd11dd0f68a5f9abe6c15bd7194dcbcdc4c87c4
Aryak Lahane <aryakprakashlahane@gmail.com>
2022-11-14 17:21:29 +0530

    feat : Like & Delete Features

--- src/frontend/components/UserCard/UserCard.js
+++ src/frontend/components/UserCard/UserCard.js
@@ -1,33 +1,72 @@
+import { useState } from "react";
+import { useUser } from "../../context/UserContext";
+import { DeleteModal } from "../DeleteModal/DeleteModal";
 import "./UserCard.css";
 
 const UserCard = ({ user }) => {
-  const { profileImg, fullname, email, phone, website, liked } = user;
+  const {
+    profileImg,
+    fullname,
+    email,
+    phone,
+    website,
+    liked,
+    id: { value },
+  } = user;
+  const { dispatchUsers } = useUser();
+
+  const [modal, setModal] = useState("");
+
   return (
-    <div className="user__card flex__column">
-      <img className="user__img" src={profileImg} alt="user__img" />
-      <div className="user__details-container flex__column">
-        <p className="text__large">{fullname}</p>
-        <div className="user__details flex__row-center">
-          <span className="material-icons-outlined icon">mail</span>
-          <p className="text__small">{email}</p>
+    <>
+      {modal && (
+        <div
+          className="modal__wrapper flex__column-center"
+          onClick={() => setModal("")}
+        >
+          {modal === "delete" ? (
+            <DeleteModal value={value} setModal={setModal} />
+          ) : (
+            <></>
+          )}
         </div>
-        <div className="user__details flex__row-center">
-          <span className="material-icons-outlined icon">call</span>
-          <p className="text__small">{phone}</p>
+      )}
+      <div className="user__card flex__column">
+        <img className="user__img" src={profileImg} alt="user__img" />
+        <div className="user__details-container flex__column">
+          <p className="text__large">{fullname}</p>
+          <div className="user__details flex__row-center">
+            <span className="material-icons-outlined icon">mail</span>
+            <p className="text__small">{email}</p>
+          </div>
+          <div className="user__details flex__row-center">
+            <span className="material-icons-outlined icon">call</span>
+            <p className="text__small">{phone}</p>
+          </div>
+          <div className="user__details flex__row-center">
+            <span className="material-icons-outlined icon">language</span>
+            <p className="text__small">{website}</p>
+          </div>
         </div>
-        <div className="user__details flex__row-center">
-          <span className="material-icons-outlined icon">language</span>
-          <p className="text__small">{website}</p>
+        <div className="user__choices flex__row-center">
+          <span
+            className="material-icons favorite"
+            onClick={() =>
+              dispatchUsers({ type: "LIKETOGGLE", payload: value })
+            }
+          >
+            {liked ? "favorite" : "favorite_outline"}
+          </span>
+          <span className="material-icons-outlined icon edit">edit</span>
+          <span
+            className="material-icons-outlined icon delete"
+            onClick={() => setModal("delete")}
+          >
+            delete
+          </span>
         </div>
       </div>
-      <div className="user__choices flex__row-center">
-        <span className="material-icons favorite">
-          {liked ? "favorite" : "favorite_outline"}
-        </span>
-        <span className="material-icons-outlined icon edit">edit</span>
-        <span className="material-icons-outlined icon delete">delete</span>
-      </div>
-    </div>
+    </>
   );
 };
 
