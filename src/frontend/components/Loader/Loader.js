b0b00067a155ca45aa6afb6e0e12cd7b95278045
Aryak Lahane <aryakprakashlahane@gmail.com>
2022-11-14 19:57:50 +0530

    feat : created loader

---
+++ src/frontend/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader__container flex__column-center">
+      <div class="spinner"></div>
+    </div>
+  );
+};
+
+export { Loader };
